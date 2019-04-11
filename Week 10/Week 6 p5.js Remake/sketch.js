4dc0960fd1c81bfcb8eefb84a32e3da4586ac212
Leire Irizarry <irizarryleire@gmail.com>
2019-04-11 16:29:28 -0400

    commit

--- Week 10/Week 6 p5.js Remake/sketch.js
+++ Week 10/Week 6 p5.js Remake/sketch.js
@@ -1,87 +1,59 @@
-// MAIN FILE Assignment_6 //
+let rain;
+let raindrops = [];
+let canvas;
+
 
 function setup(){
-  let rain = Rain(200, 200);
-  createCanvas(400, 400);
-  background(0);
+  canvas = createCanvas(400, 400);
+  background(100);
+  rain = new Rain(200, 200);
 }
 
 function draw(){
   background(100);
   drawScenery();
-  drawCloud(mouseX, mouseY);
+  let x = mouseX;
+  let y = mouseY;
   rain.addDrop();
   rain.start();
+  drawCloud(x, y);
 }
 
-// ***************************************** //
 
-// Rain class //
-class Rain {
-  var raindrops = []*12;
-  let position = createVector(200, 200);
 
-  constructor(x, y){
-    position.x = x;
-    position.y = y;
-  }
-
-  // begin the actual raining
-  start(){
-    for(let i = 0; i < raindrops.length; i++){
-      let raindrop = raindrops[i];
-      raindrop.show();
-      raindrop.move();
-
-      if(raindrop.isDead()){
-        raindrops.remove(i);
-      }
-    }
-  }
-
-  // add new raindrops to array
-  addDrop() {
-    raindrops.add(Raindrop(position.x, position.y));
-  }
-}
-
-// ***************************************** //
-
-// Raindrop clas //
+// Raindrop class
 class Raindrop {
-  let vel;
-  let spd;
-  let position;
-
-  var size = random(5, 10);
-  var duration = 255;
 
   constructor(x, y){
-    position = createVector(random(mouseX-30, mouseX+30), mouseY+28);
-    spd = createVector(0, .1);
-    vel = createVector(0, .1);
+
+    this.size = random(5,10);
+    this.duration = 255;
+
+
+    this.position = createVector(random(mouseX-30, mouseX+30), mouseY+28);
+    this.spd = createVector(0, .1);
+    this.vel = createVector(0, .1);
   }
 
-  // methods
   move(){
-    fall();
-    show();
+    this.fall();
+    this.show();
   }
 
   fall(){
-    vel.add(spd);
-    position.add(vel);
-    duration -= 4;
+    this.vel.add(this.spd);
+    this.position.add(this.vel);
+    this.duration -= 4;
   }
 
   show(){
     noStroke();
-    fill(random(0,100), random(0, 255), 255, duration);
-    circle(position.x, position.y, size);
+    fill(random(0, 100), random(0, 255), 255, this.duration);
+    circle(this.position.x, this.position.y, this.size);
   }
 
   isDead(){
-    if (duration < 0){
+    if (this.duration < 0){
       return true;
     }else{
       return false;
@@ -89,31 +61,57 @@ class Raindrop {
   }
 }
 
-// ***************************************** //
-// FUNCTIONS //
+// Rain class
+class Rain {
 
-// drawCloud function //
-drawCloud(x, y){
-  let cloud = loadImage("cloud.png");
-  imageMode(CENTER);
-  image(cloud, x, y);
+  constructor(x, y) {
+
+    this.position = createVector(200, 200);
+    this.position.x = x;
+    this.position.y = y;
+  }
+
+  addDrop(){
+    raindrops.push(new Raindrop(this.position.x, this.position.y));
+  }
+
+  start(){
+    for (let i = 0; i < raindrops.length; i++){
+      let raindrop = raindrops[i];
+      raindrop.show();
+      raindrop.move();
+
+      if (raindrop.isDead()){
+        raindrops.splice(i, 1);
+      }
+    }
+  }
 }
 
-// drawScenery function //
-drawScenery(){
 
-  // grass
+// drawScenery
+
+
+function drawScenery(){
   noStroke();
   fill(49, 109, 49);
-  rect(0, 350, 400, 50);
+  rect(0, 350, width, 50);
 
-  // house
   fill(255, 252, 229);
   rect(30, 260, 100, 90);
 
-  // roof
   fill(198, 58, 29);
   triangle(30, 260,
-           80, 210,
-           130, 260);
+         80, 210,
+         130, 260);
+}
+
+function drawCloud(x, y){
+  fill(230);
+  ellipse(x - 10, y + 10, 70, 40);
+  ellipse(x + 10, y + 10, 70, 40);
+  circle(x - 15, y, 20);
+  circle(x + 15, y, 20);
+  ellipse(x, y, 50, 50);
+
 }
